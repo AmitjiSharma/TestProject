69c0c097c27380b5d56d664b9b9b9ab8dcd0fa8e
AmitjiSharma <mr.amitjisharma@gmail.com>
2021-06-29 11:12:51 +0530

    1 file modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor(private _employee : emp) { }
-
+  constructor(private _employee : emp) { 
   
 
   ngOnInit() {
