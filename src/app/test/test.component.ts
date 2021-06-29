b933691d19c728358476d7989b1f73d244e659ce
AmitjiSharma <mr.amitjisharma@gmail.com>
2021-06-29 11:19:52 +0530

    2 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+    <ul *ngFor="let data of list">
+      <h1>{{ data.name }}</h1>
+    </ul>
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  list = [];
 
-  constructor(private _employee : emp) { 
-  
+  constructor(private _employee: ServiceService) {}
 
   ngOnInit() {
+    this.list = this._employee.getEmployees();
   }
-
 }
